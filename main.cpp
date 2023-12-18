6fd5764fd21bd1d0c97b9c7f5552227c38edbf6c
Tommy Le <tommyle1624@gmail.com>
2023-12-18 11:06:06 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,32 @@
+#include <string>
+#include <iostream>
+
 class Solution {
 public:
     int totalMoney(int n) {
-        int deposit = 1;
+        int deposit = 1; //First Monday
         int amount = 0;
+
         for (int i = 0; i < n; i++) {
             amount += deposit;
-            deposit++;
-            if (i % 7 == 0) {
-                deposit = 1;  // Reset deposit to 1 on Monday
+            deposit++; //from Tuesday to Sunday, he is putting 1 more in than the previous day
+
+            // Check if it's Monday as i starts at 0 so it ensures i+1 will be 7 at 6
+            if ((i + 1) % 7 == 0) {
+                deposit = i / 7 + 2;  // Increment deposit on each following Monday so like on 7 for next Monday, it would be 2
+                //std::cout << "deposit: " << deposit << std::endl;
             }
         }
+
         return amount;
     }
 };
 
-//This is what I have so far but it's not working
-// I sort of took sleeping pills so I can't really think so I'm gonna work on it tmr
+int main()
+{
+    Solution bank;
+    int days = 0;
+    std::cout << "How much days will Hercy deposit his money? " << std::endl;
+    std::cin >> days;
+    std::cout << "\nThe Amount at the moment: $" << bank.totalMoney(days);
+}
